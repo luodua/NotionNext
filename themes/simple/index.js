926121ac0af209ea1d21ed1a5d80f9c328eca80c
luodua <47820184+luodua@users.noreply.github.com>
2025-07-12 19:58:14 +0800

    Update index.js

--- themes/simple/index.js
+++ themes/simple/index.js
@@ -79,7 +79,8 @@ const LayoutBase = props => {
 
         {siteConfig('SIMPLE_TOP_BAR', null, CONFIG) && <TopBar {...props} />}
 
-
+        {/* 顶部LOGO */}
+        <Header {...props} />
 
         {/* 导航栏 */}
         <NavBar {...props} />
